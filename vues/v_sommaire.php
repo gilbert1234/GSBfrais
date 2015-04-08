7c99cfe609e596ef56ef313fcb9e45657e2fdef3
unknown <gtrinidad@BJ05-PC10.infogest.enc>
2015-04-08 14:48:09 +0200

    J'ai modifié "Visiteur :<br> dans le fichier v_sommaire dans la vue en "Bonjour <br>"

--- vues/v_sommaire.php
+++ vues/v_sommaire.php
@@ -9,7 +9,7 @@
 		</div>  
         <ul id="menuList">
 			<li >
-				Visiteur :<br>
+				<!--Visiteur-->Bonjour :<br>
 				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
 			</li>
 			<li class="smenu">
